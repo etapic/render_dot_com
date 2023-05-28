0d514b043282d9c3b2c7f81f07a953a481fe3a54
etapic <82479880+etapic@users.noreply.github.com>
2023-05-28 21:37:39 +0200

    Update to get linux version

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+
+const { exec } = require("child_process");
+
+//getting linux version
+exec("cat /proc/version", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Test app listening on port ${port}!`));
